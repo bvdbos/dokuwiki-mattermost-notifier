18b7a72689dc679fef8ac81da1eafe4b72a36634
Bas van den Bosch <bvdbos@gmail.com>
2017-04-28 07:11:08 +0200

    Update default.php

--- conf/default.php
+++ conf/default.php
@@ -4,4 +4,4 @@
         $conf['notify_edit'] = 1;
         $conf['notify_delete'] = 1;
         $conf['show_summary'] = 1;
-        $conf['webhook']  = "https://hooks.slack.com/services/<token>";
+        $conf['webhook']  = "http://{your-mattermost-site}/hooks/xxx-generatedkey-xxx";
